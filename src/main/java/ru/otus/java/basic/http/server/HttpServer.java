0d77f1c1c55415e9621e4a120d957b285b9b8aeb
VladLisitsyn <vladlisitsindiklis@yandex.ru>
2024-02-11 18:54:51 +0800

    homework

--- src/main/java/ru/otus/java/basic/http/server/HttpServer.java
+++ src/main/java/ru/otus/java/basic/http/server/HttpServer.java
@@ -3,32 +3,53 @@ package ru.otus.java.basic.http.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
     private int port;
     private Dispatcher dispatcher;
+    private ExecutorService threadPool;
 
     public HttpServer(int port) {
         this.port = port;
         this.dispatcher = new Dispatcher();
+        this.threadPool = Executors.newFixedThreadPool(10);
     }
 
     public void start() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Сервер запущен на порту: " + port);
             while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    byte[] buffer = new byte[8192];
-                    int n = socket.getInputStream().read(buffer);
-                    String rawRequest = new String(buffer, 0, n);
-                    HttpRequest httpRequest = new HttpRequest(rawRequest);
-                    dispatcher.execute(httpRequest, socket.getOutputStream());
+                try {
+                    Socket socket = serverSocket.accept();
+                    threadPool.execute(() -> handleClient(socket));
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            threadPool.shutdown();
+        }
+    }
+
+    private void handleClient(Socket socket) {
+        try {
+            byte[] buffer = new byte[8192];
+            int n = socket.getInputStream().read(buffer);
+            String rawRequest = new String(buffer, 0, n);
+            HttpRequest httpRequest = new HttpRequest(rawRequest);
+            dispatcher.execute(httpRequest, socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
